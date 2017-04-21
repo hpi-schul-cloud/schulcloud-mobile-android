00b4b89024607c7156f333bacfdf7b4d64b711ca
Lars Lange <lars.lange@student.hpi.de>
2017-04-21 16:37:31 +0200

    WIP

--- app/src/main/java/org/schulcloud/mobile/ui/base/BaseActivity.java
+++ app/src/main/java/org/schulcloud/mobile/ui/base/BaseActivity.java
@@ -1,19 +1,36 @@
 package org.schulcloud.mobile.ui.base;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.util.LongSparseArray;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.schulcloud.mobile.R;
 import org.schulcloud.mobile.SchulCloudApplication;
 import org.schulcloud.mobile.injection.component.ActivityComponent;
 import org.schulcloud.mobile.injection.component.ConfigPersistentComponent;
 import org.schulcloud.mobile.injection.component.DaggerConfigPersistentComponent;
 import org.schulcloud.mobile.injection.module.ActivityModule;
+import org.schulcloud.mobile.ui.main.MainActivity;
+
 import timber.log.Timber;
 
 public class BaseActivity extends AppCompatActivity {
+
+    protected DrawerLayout mDrawer;
+    protected ListView drawerList;
+    protected ActionBarDrawerToggle drawerToggle;
 
     private static final String KEY_ACTIVITY_ID = "KEY_ACTIVITY_ID";
     private static final AtomicLong NEXT_ID = new AtomicLong(0);
@@ -25,6 +42,23 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.drawer_layout);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ArrayList<DrawerListItem> drawerListItems = new ArrayList<DrawerListItem>();
+        drawerListItems.add(new DrawerListItem(0,"AIR° DEVICES"));
+        drawerListItems.add(new DrawerListItem(1,"A/C Device [1]"));
+        drawerListItems.add(new DrawerListItem(1,"A/C Device [2]"));
+        drawerListItems.add(new DrawerListItem(1,"A/C Device [3]"));
+        drawerListItems.add(new DrawerListItem(0,"AIR° FEATURES"));
+        drawerListItems.add(new DrawerListItem(2,"SLEEP MODE"));
+        drawerListItems.add(new DrawerListItem(2,"TRACKING MODE"));
+        drawerListItems.add(new DrawerListItem(2,"SETTINGS"));
+        DrawerAdapter mDrawerAdapter = new DrawerAdapter(this, R.layout.drawer_list_item, drawerListItems);
+        ListView mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(mDrawerAdapter);
 
         // Create the ActivityComponent and reuses cached ConfigPersistentComponent if this is
         // being called after a configuration change.
---
+++ app/src/main/java/org/schulcloud/mobile/ui/base/DrawerAdapter.java
@@ -0,0 +1,10 @@
+package org.schulcloud.mobile.ui.base;
+//https://github.com/acomminos/Plumble/blob/master/app/src/main/java/com/morlunk/mumbleclient/app/DrawerAdapter.java
+//http://stackoverflow.com/questions/21405958/how-to-display-navigation-drawer-in-all-activities
+
+public class DrawerAdapter {
+
+    public class DrawerListItem {
+
+    }
+}

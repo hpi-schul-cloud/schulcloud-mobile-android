60480312e2720ea649e6ed4ab80cc353319bdfbe
Lars Lange <langleu90@gmail.com>
2017-05-22 13:02:03 +0200

    Update upload-apk.sh

--- .travis/upload-apk.sh
+++ .travis/upload-apk.sh
@@ -10,8 +10,8 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
     if [ -z "$TRAVIS_TAG" ]; then
       echo -e "Starting to tag commit.\n"
 
-      git config --global user.email $EMAIL
-      git config --global user.name $USERNAME
+	    git config --global user.email "travis@travis-ci.org"
+	    git config --global user.name "Travis"
 
       # Add tag and push to master.
       git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."

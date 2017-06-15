cd8bd405f974725eb09678da133fc06127c4918d
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-06-15 11:15:16 +0200

    remove unnecessary import

--- app/src/main/java/org/schulcloud/mobile/data/model/Directory.java
+++ app/src/main/java/org/schulcloud/mobile/data/model/Directory.java
@@ -1,7 +1,5 @@
 package org.schulcloud.mobile.data.model;
 
-import org.parceler.Transient;
-
 import io.realm.RealmModel;
 import io.realm.annotations.PrimaryKey;
 import io.realm.annotations.RealmClass;

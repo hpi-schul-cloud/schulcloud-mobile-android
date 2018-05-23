dc5d676e01810cb2d88c62c7f5de040ab91575ca
Andrea Nathansen <andrea.nathansen@student.hpi.uni-potsdam.de>
2018-05-23 12:02:29 +0200

    set up NewsListAdapter and NewsListFragment with dummy data

---
+++ app/src/main/java/org/schulcloud/mobile/controllers/main/NewsListAdapter.kt
@@ -0,0 +1,46 @@
+package org.schulcloud.mobile.controllers.main
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.item_news_list.view.*
+import org.schulcloud.mobile.R
+import org.schulcloud.mobile.models.news.News
+
+class NewsListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var newsList: List <News> = emptyList()
+
+    fun update (newsList: List<News>){
+        this.newsList=newsList
+        //notifyDataSetChanged()
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news_list, parent, false)
+        return NewsViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return newsList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val news: News = newsList[position]
+        if (holder is NewsViewHolder){
+            holder.newsTitle.text = news.title
+            holder.newsDate.text = news.createdAt
+            if (news.content!= null) {
+                holder.newsContent.text = news.content!!.replace("\n", " ")
+            }
+        }
+    }
+
+    class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        var newsTitle: TextView = view.findViewById(R.id.news_title)
+        var newsDate: TextView = view.findViewById(R.id.news_date)
+        var newsContent: TextView = view.findViewById(R.id.news_content)
+    }
+
+}
--- app/src/main/java/org/schulcloud/mobile/controllers/main/NewsListFragment.kt
+++ app/src/main/java/org/schulcloud/mobile/controllers/main/NewsListFragment.kt
@@ -3,11 +3,14 @@ package org.schulcloud.mobile.controllers.main
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.schulcloud.mobile.R
 import org.schulcloud.mobile.controllers.base.BaseFragment
+import org.schulcloud.mobile.models.news.News
 
 class NewsListFragment : BaseFragment() {
 
@@ -15,10 +18,36 @@ class NewsListFragment : BaseFragment() {
         val TAG: String = NewsListFragment::class.java.simpleName
     }
 
+    private var newsListAdapter: NewsListAdapter? = null
+    //dummy data
+    private var newsData : List<News> = listOf (
+            News ("title 1", "01.01.2018", "content 1"),
+            News ("title 2", "02.02.2018", "content 2 content\n content\n content\n content content"),
+            News ("title 3", "03.03.2018", "content 3"),
+            News ("title 4", "04.04.2018", "content 4"))
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //set viewModel here
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         activity?.title = getString(R.string.title_list_news)
         return inflater.inflate(R.layout.fragment_news_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = activity!!.findViewById<RecyclerView>(R.id.recycler_view_news)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        newsListAdapter = NewsListAdapter()
+
+        //modify to handle ViewModels
+        newsListAdapter!!.update(newsData!!)
+
+        recyclerView.adapter = newsListAdapter
     }
 
 

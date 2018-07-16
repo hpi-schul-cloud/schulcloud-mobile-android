0ecfe1633e1070a332a984d0358188d72051e7b0
Elias Lehnert <araknor99@Eliass-MBP.fritz.box>
2018-07-16 19:56:00 +0200

    added rotation animations for devices list chevron, adding animation logic to devices list

--- app/src/main/java/org/schulcloud/mobile/controllers/main/DeviceListAdapter.kt
+++ app/src/main/java/org/schulcloud/mobile/controllers/main/DeviceListAdapter.kt
@@ -1,18 +1,51 @@
 package org.schulcloud.mobile.controllers.main
 
+import android.support.v7.widget.RecyclerView
+import android.transition.Visibility
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import org.schulcloud.mobile.R
 import org.schulcloud.mobile.controllers.base.BaseAdapter
 import org.schulcloud.mobile.controllers.base.BaseViewHolder
 import org.schulcloud.mobile.controllers.base.OnItemSelectedCallback
 import org.schulcloud.mobile.databinding.ItemDeviceBinding
 import org.schulcloud.mobile.models.devices.Device
 
-class DeviceListAdapter(private val selectedCallback: OnItemSelectedCallback):
+class DeviceListAdapter(private val selectedCallback: OnItemSelectedCallback,private val mChevron: ImageView,private val mEmpty: TextView):
         BaseAdapter<Device,DeviceListAdapter.DeviceViewHolder,ItemDeviceBinding>(){
 
+    private var active = false
+    private val states = arrayOf(R.anim.rotate_0_180,R.anim.rotate_180_0)
+    private lateinit var mRecyclerGroup: ViewGroup
+    private lateinit var animationListener: Animation.AnimationListener
+
+    fun Boolean.toInt() = if (this) 1 else 0
+
     fun update(deviceList: List<Device>){
-        items = deviceList
+        if(active)
+            items = deviceList
+        else
+            items = ArrayList<Device>()
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        mRecyclerGroup = recyclerView.parent as ViewGroup
+        super.onAttachedToRecyclerView(recyclerView)
+    }
+
+    fun changeState(deviceList: List<Device>){
+        mChevron.startAnimation(AnimationUtils.loadAnimation(mChevron.context,states[active.toInt()]))
+        active = !active
+        if(deviceList.isEmpty() && active)
+            mEmpty.visibility = View.VISIBLE
+        else
+            mEmpty.visibility = View.GONE
+        update(deviceList)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceListAdapter.DeviceViewHolder {

61ab0c91ac5195ce277e46e41a86ae3a88aad71a
Andrea Nathansen <andrea.nathansen@student.hpi.uni-potsdam.de>
2018-07-18 11:08:56 +0200

    sync news data on user change

--- app/src/main/java/org/schulcloud/mobile/models/user/UserRepository.kt
+++ app/src/main/java/org/schulcloud/mobile/models/user/UserRepository.kt
@@ -8,6 +8,7 @@ import org.schulcloud.mobile.jobs.CreateAccessTokenJob
 import org.schulcloud.mobile.jobs.base.RequestJobCallback
 import org.schulcloud.mobile.models.Credentials
 import org.schulcloud.mobile.models.course.CourseRepository
+import org.schulcloud.mobile.models.news.NewsRepository
 import org.schulcloud.mobile.storages.UserStorage
 
 object UserRepository {
@@ -34,6 +35,7 @@ object UserRepository {
 
             // Sync data in background
             CourseRepository.syncCourses()
+            NewsRepository.syncNews()
         }
     }
 

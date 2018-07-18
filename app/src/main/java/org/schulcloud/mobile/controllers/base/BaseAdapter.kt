0577668d1ed3d9d1d1b5eb5de580bfb4ae92b6c5
Jonas Wanke <jo.wanke2000@gmail.com>
2018-07-18 17:00:54 +0200

    feat(base): hide RecyclerView if empty

--- app/src/main/java/org/schulcloud/mobile/controllers/base/BaseAdapter.kt
+++ app/src/main/java/org/schulcloud/mobile/controllers/base/BaseAdapter.kt
@@ -14,6 +14,20 @@ abstract class BaseAdapter<T : Any, VH : BaseViewHolder<T, B>, out B : ViewDataB
     var items: List<T> by Delegates.observable(emptyList()) { _, _, _ ->
         notifyDataSetChanged()
         emptyIndicator?.visibility = items.isEmpty().asVisibility()
+        recyclerView?.visibility = items.isNotEmpty().asVisibility()
+    }
+
+    var recyclerView: RecyclerView? = null
+        private set
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
+    }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        this.recyclerView = null
+        super.onDetachedFromRecyclerView(recyclerView)
     }
 
     override fun getItemCount(): Int = items.size

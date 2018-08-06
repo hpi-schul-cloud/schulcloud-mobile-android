f6b417da708dfa24a157e8ba81c71a10f573b372
Jonas Wanke <contact@jonas-wanke.com>
2018-08-06 15:03:21 +0200

    fix(dashboard/news): remove RelativeLayout

--- app/src/main/java/org/schulcloud/mobile/views/NoScrollLinearLayoutManager.kt
+++ app/src/main/java/org/schulcloud/mobile/views/NoScrollLinearLayoutManager.kt
@@ -4,12 +4,12 @@ import android.content.Context
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class NoScrollLinearLayoutManager(context: Context,
-                                  @RecyclerView.Orientation orientation: Int = VERTICAL,
-                                  reverseLayout: Boolean = false)
-    : LinearLayoutManager(context, orientation, reverseLayout) {
+class NoScrollLinearLayoutManager(
+    context: Context,
+    @RecyclerView.Orientation orientation: Int = VERTICAL,
+    reverseLayout: Boolean = false
+) : LinearLayoutManager(context, orientation, reverseLayout) {
 
-    override fun canScrollVertically(): Boolean {
-        return false
-    }
+    override fun canScrollVertically() = false
+    override fun isAutoMeasureEnabled() = true
 }

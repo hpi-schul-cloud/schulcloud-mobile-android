21b634d876b770bfb3acc0bdb6194db80c53c4c1
Jonas Wanke <contact@jonas-wanke.com>
2018-08-20 08:18:56 +0200

    refactor(file): rename ItemFileCourse to ItemFileCourse

--- app/src/main/java/org/schulcloud/mobile/controllers/file/FileOverviewCourseAdapter.kt
+++ app/src/main/java/org/schulcloud/mobile/controllers/file/FileOverviewCourseAdapter.kt
@@ -4,23 +4,23 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import org.schulcloud.mobile.controllers.base.BaseAdapter
 import org.schulcloud.mobile.controllers.base.BaseViewHolder
-import org.schulcloud.mobile.databinding.ItemFileCourseBinding
+import org.schulcloud.mobile.databinding.ItemDirectoryCourseBinding
 import org.schulcloud.mobile.models.course.Course
 
 class FileOverviewCourseAdapter(private val onSelected: (String) -> Unit)
-    : BaseAdapter<Course, FileOverviewCourseAdapter.CourseViewHolder, ItemFileCourseBinding>() {
+    : BaseAdapter<Course, FileOverviewCourseAdapter.CourseViewHolder, ItemDirectoryCourseBinding>() {
 
     fun update(courseList: List<Course>) {
         items = courseList
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
-        val binding = ItemFileCourseBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemDirectoryCourseBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         binding.onSelected = onSelected
         return CourseViewHolder(binding)
     }
 
-    class CourseViewHolder(binding: ItemFileCourseBinding) : BaseViewHolder<Course, ItemFileCourseBinding>(binding) {
+    class CourseViewHolder(binding: ItemDirectoryCourseBinding) : BaseViewHolder<Course, ItemDirectoryCourseBinding>(binding) {
         override fun onItemSet() {
             binding.course = item
         }

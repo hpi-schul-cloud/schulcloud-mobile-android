4fccaa428131a99c30c1f33184d2d600d40d28a1
Andrea Nathansen <andrea.nathansen@student.hpi.uni-potsdam.de>
2018-09-06 14:08:04 +0200

    refactor(ApiService): clean up code

--- app/src/main/java/org/schulcloud/mobile/network/ApiService.kt
+++ app/src/main/java/org/schulcloud/mobile/network/ApiService.kt
@@ -1,6 +1,7 @@
 package org.schulcloud.mobile.network
 
-import android.util.Log
+import android.os.Build
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import info.guardianproject.netcipher.client.TlsOnlySocketFactory
 import okhttp3.OkHttpClient
@@ -13,18 +14,15 @@ import org.schulcloud.mobile.models.user.UserRepository
 import org.schulcloud.mobile.utils.HOST
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.BufferedInputStream
-import java.io.FileInputStream
 import java.io.InputStream
 import java.security.KeyStore
-import java.security.cert.Certificate
 import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
 import java.util.*
 import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManager
 import javax.net.ssl.TrustManagerFactory
 import javax.net.ssl.X509TrustManager
-import java.security.cert.X509Certificate
 
 
 object ApiService {
@@ -33,83 +31,44 @@ object ApiService {
 
     @Synchronized
     fun getInstance(): ApiServiceInterface {
+        val client = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+            getTlsClient(null as KeyStore?)
+        else
+            getOkHttpClientBuilder().build()
 
-        val loggingInterceptor = HttpLoggingInterceptor()
-        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-
-        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()).apply {
-            init(null as KeyStore?)
-        }
-        val trustManagers = trustManagerFactory.getTrustManagers()
-        if (trustManagers.size != 1 || trustManagers[0] !is X509TrustManager) {
-            throw IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers))
-        }
-        val trustManager = trustManagers[0] as X509TrustManager
-
-        val sslContext = SSLContext.getInstance("TLS")
-        sslContext.init(null, arrayOf<TrustManager>(trustManager), null)
-
-        val client = OkHttpClient.Builder()
-                .sslSocketFactory(TlsOnlySocketFactory(sslContext.socketFactory), trustManager)
-                .addInterceptor { chain ->
-                    val request = chain.request()
-                    val builder = request.newBuilder()
-                    if (UserRepository.isAuthorized
-                            && request.url().host().equals(HOST.substringAfterLast('/'), true)) {
-                        builder.header(Config.HEADER_AUTH, Config.HEADER_AUTH_VALUE_PREFIX + UserRepository.token)
-                    }
-                    chain.proceed(builder.build())
-                }
-                .addInterceptor(loggingInterceptor)
-                .build()
-
-        val gson = GsonBuilder()
-                .create()
-
-        val retrofit = Retrofit.Builder()
-                .baseUrl(BuildConfig.API_URL)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build()
-
-        service = retrofit.create(ApiServiceInterface::class.java)
+        service = getRetrofit(client).create(ApiServiceInterface::class.java)
         return service as ApiServiceInterface
     }
 
     @Synchronized
     fun getFileDownloadInstance(): ApiServiceInterface {
+        val client = getTlsClient(getFileDownloadKeyStore())
+        service = getRetrofit(client).create(ApiServiceInterface::class.java)
+        return service as ApiServiceInterface
+    }
 
-        val loggingInterceptor = HttpLoggingInterceptor()
-        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-
+    // To use T-TeleSec GlobalRoot Class 2 certificate on pre-21
+    // https://developer.android.com/training/articles/security-ssl
+    private fun getFileDownloadKeyStore(): KeyStore {
         val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
-
-        val caInput: InputStream = SchulCloudApp.instance.resources.openRawResource(R.raw.globalroot_class_2)
+        val caInput: InputStream = SchulCloudApp.instance.resources
+                .openRawResource(R.raw.globalroot_class_2)
         val ca: X509Certificate = caInput.use {
             cf.generateCertificate(it) as X509Certificate
         }
-        Log.i("CA","ca=" + ca.subjectDN)
-
         val keyStoreType = KeyStore.getDefaultType()
-        val keyStore = KeyStore.getInstance(keyStoreType).apply {
+
+        return KeyStore.getInstance(keyStoreType).apply {
             load(null, null)
-                setCertificateEntry("ca", ca)
+            setCertificateEntry("globalroot_class_2", ca)
         }
+    }
 
-        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()).apply {
-            init(keyStore)
-        }
-        val trustManagers = trustManagerFactory.getTrustManagers()
-        if (trustManagers.size != 1 || trustManagers[0] !is X509TrustManager) {
-            throw IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers))
-        }
-        val trustManager = trustManagers[0] as X509TrustManager
+    private fun getOkHttpClientBuilder(): OkHttpClient.Builder {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
 
-        val sslContext = SSLContext.getInstance("TLS")
-        sslContext.init(null, arrayOf<TrustManager>(trustManager), null)
-
-        val client = OkHttpClient.Builder()
-                .sslSocketFactory(TlsOnlySocketFactory(sslContext.socketFactory), trustManager)
+        return OkHttpClient.Builder()
                 .addInterceptor { chain ->
                     val request = chain.request()
                     val builder = request.newBuilder()
@@ -120,18 +79,42 @@ object ApiService {
                     chain.proceed(builder.build())
                 }
                 .addInterceptor(loggingInterceptor)
-                .build()
+    }
 
-        val gson = GsonBuilder()
+    private fun getGson(): Gson {
+        return GsonBuilder()
                 .create()
+    }
 
-        val retrofit = Retrofit.Builder()
+    private fun getRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
                 .baseUrl(BuildConfig.API_URL)
                 .client(client)
-                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addConverterFactory(GsonConverterFactory.create(getGson()))
                 .build()
+    }
 
-        service = retrofit.create(ApiServiceInterface::class.java)
-        return service as ApiServiceInterface
+    // To enable and use TLS 1.1 and 1.2 on pre-21
+    // https://square.github.io/okhttp/3.x/okhttp/okhttp3/OkHttpClient.Builder
+    private fun getTlsClient(keyStore: KeyStore?): OkHttpClient {
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()).apply {
+            init(keyStore)
+        }
+        val trustManager = getTrustManager(trustManagerFactory)
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, arrayOf<TrustManager>(trustManager), null)
+
+        return getOkHttpClientBuilder()
+                .sslSocketFactory(TlsOnlySocketFactory(sslContext.socketFactory), trustManager)
+                .build()
+    }
+
+    @Throws(IllegalStateException::class)
+    private fun getTrustManager(trustManagerFactory: TrustManagerFactory): X509TrustManager {
+        val trustManagers = trustManagerFactory.getTrustManagers()
+        if (trustManagers.size != 1 || trustManagers[0] !is X509TrustManager) {
+            throw IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers))
+        }
+        return trustManagers[0] as X509TrustManager
     }
 }

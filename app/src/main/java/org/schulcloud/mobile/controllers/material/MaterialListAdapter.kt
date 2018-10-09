c9680e828af69ff5acabd510c6337b74db2f381b
Andrea Nathansen <andrea.nathansen@student.hpi.uni-potsdam.de>
2018-10-09 16:09:50 +0200

    feat(material): add MaterialListFragment with adapter and layout files

---
+++ app/src/main/java/org/schulcloud/mobile/controllers/material/MaterialListAdapter.kt
@@ -0,0 +1,28 @@
+package org.schulcloud.mobile.controllers.material
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import org.schulcloud.mobile.controllers.base.BaseAdapter
+import org.schulcloud.mobile.controllers.base.BaseViewHolder
+import org.schulcloud.mobile.databinding.ItemMaterialBinding
+import org.schulcloud.mobile.models.material.Material
+
+class MaterialListAdapter
+    : BaseAdapter<Material, MaterialListAdapter.MaterialViewHolder, ItemMaterialBinding>() {
+
+    fun update(materials: List<Material>) {
+        items = materials
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MaterialViewHolder {
+        val binding = ItemMaterialBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MaterialViewHolder(binding)
+    }
+
+    class MaterialViewHolder(binding: ItemMaterialBinding) : BaseViewHolder<Material, ItemMaterialBinding>(binding) {
+        override fun onItemSet() {
+            binding.material = item
+            binding.viewHolder = this
+        }
+    }
+}
---
+++ app/src/main/java/org/schulcloud/mobile/controllers/material/MaterialListFragment.kt
@@ -0,0 +1,70 @@
+package org.schulcloud.mobile.controllers.material
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_material_list.*
+import org.schulcloud.mobile.R
+import org.schulcloud.mobile.controllers.main.FragmentType
+import org.schulcloud.mobile.controllers.main.MainFragment
+import org.schulcloud.mobile.controllers.main.MainFragmentConfig
+
+import org.schulcloud.mobile.models.material.MaterialRepository
+import org.schulcloud.mobile.utils.asLiveData
+import org.schulcloud.mobile.viewmodels.MaterialListViewModel
+
+class MaterialListFragment : MainFragment<MaterialListViewModel>() {
+
+    companion object {
+        val TAG: String = MaterialListFragment::class.java.simpleName
+    }
+
+    private val materialAdapter: MaterialListAdapter by lazy {
+        MaterialListAdapter()
+    }
+
+    override var url: String? = "/content"
+    override fun provideConfig(): LiveData<MainFragmentConfig> = MainFragmentConfig(
+            fragmentType = FragmentType.PRIMARY,
+            title = "Lernstore"
+    ).asLiveData()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MaterialListViewModel::class.java)
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_material_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        materialAdapter.emptyIndicator = empty
+        viewModel.materials.observe(this, Observer {
+            materialAdapter.update(it ?: emptyList())
+        })
+
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = materialAdapter
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        }
+    }
+
+    override suspend fun refresh() {
+        MaterialRepository.syncMaterials()
+    }
+}

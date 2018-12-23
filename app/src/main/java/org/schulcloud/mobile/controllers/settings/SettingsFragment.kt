0b902aab999f1dbf56c1f30efe512b4ed99d1316
Jonas Wanke <contact@jonas-wanke.com>
2018-12-23 17:58:09 +0100

    ui(settings): only show theme-darkMode-ambientLight if supported

--- app/src/main/java/org/schulcloud/mobile/controllers/settings/SettingsFragment.kt
+++ app/src/main/java/org/schulcloud/mobile/controllers/settings/SettingsFragment.kt
@@ -7,5 +7,9 @@ import org.schulcloud.mobile.R
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        val context = context ?: return
+        if (DarkModeUtils.getInstance(context).supportsAmbientLight)
+            findPreference<Preference>(Preferences.Theme.DARK_MODE_AMBIENT_LIGHT).isVisible = true
     }
 }

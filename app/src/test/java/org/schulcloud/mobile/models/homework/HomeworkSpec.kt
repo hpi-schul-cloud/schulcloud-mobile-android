9e108468e734fca14894f5b814617ae93e3abb0b
Andrea Nathansen <andrea.nathansen@student.hpi.uni-potsdam.de>
2019-01-25 15:49:24 +0100

    test: add MockK framework dependency for mocking in Kotlin, HomeworkSpec still fails

--- app/src/test/java/org/schulcloud/mobile/models/homework/HomeworkSpec.kt
+++ app/src/test/java/org/schulcloud/mobile/models/homework/HomeworkSpec.kt
@@ -1,12 +1,14 @@
 package org.schulcloud.mobile.models.homework
 
+import io.mockk.every
+import io.mockk.mockkObject
 import io.realm.RealmList
 import org.schulcloud.mobile.models.base.RealmString
+import org.schulcloud.mobile.models.user.UserRepository
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
-import kotlin.test.assertNull
+import kotlin.test.*
+
 
 private const val ID = "id"
 private const val TEACHERID = "teacherId"
@@ -18,21 +20,17 @@ private const val RESTRICTED = true
 private const val PUBLICSUBMISSIONS = true
 private const val INVALID_DUEDATE = "invDueDate"
 private const val VALID_DUEDATE = "2020-07-12T10:10:10.001Z"
-private const val USERID_TEACHER = "teacherId"
+private const val USERID = "UserId"
 private const val USERID_TEACHER_OTHER = "teacherIdOther"
 private val COURSE_SUBSTITUTION_TEACHER = HomeworkCourse().apply {
     substitutionIds = RealmList(RealmString("teacherIdOther"))
 }
-/*val DUEDATE_DAY_DIFFERENCE = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-                .print(LocalDateTime.now().plusDays(2))
-        val DUEDATE_HOUR_DIFFERENCE = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-                .print(LocalDateTime.now().plusHours(3).plusMinutes(1))*//*
-
-        const val DUETIMESPANDAYS = 2
-        const val DUETIMESPANHOURS = 3*/
 
 object HomeworkSpec : Spek({
     describe("A homework") {
+        mockkObject(UserRepository)
+        every{UserRepository.userId} answers {USERID}
+
         val homework by memoized {
             Homework().apply {
                 id = ID
@@ -47,6 +45,7 @@ object HomeworkSpec : Spek({
         }
 
         describe("property access") {
+
             it("should return the assigned value") {
                 assertEquals(ID, homework.id)
                 assertEquals(TEACHERID, homework.teacherId)
@@ -67,6 +66,11 @@ object HomeworkSpec : Spek({
             it("dueDateTime should not be null") {
                 assertNotNull(homework.dueDateTime)
             }
+
+            it("timespans should not be null") {
+                assertNotNull(homework.dueTimespanHours)
+                assertNotNull(homework.dueTimespanDays)
+            }
         }
 
         describe("setting invalid dueDate") {
@@ -77,7 +81,43 @@ object HomeworkSpec : Spek({
             it("dueDateTime should be null") {
                 assertNull(homework.dueDateTime)
             }
+
+            it("timespans should be null") {
+                assertNull(homework.dueTimespanHours)
+                assertNull(homework.dueTimespanDays)
+            }
         }
-        // TODO: add tests for dueTimespans, isTeacher, canSeeSubmissions, mock context for jodatime
+
+        describe("current user is teacher") {
+            it("isTeacher should be true") {
+                assertTrue(homework.isTeacher(TEACHERID))
+            }
+        }
+        // TODO: mock dependencies
+
+        describe("making homework not restricted") {
+            beforeEach {
+                homework.restricted = false
+
+            }
+
+            describe("making submissions public") {
+                beforeEach {
+                    homework.publicSubmissions = true
+                }
+                it("user should be able to see submissions") {
+                    assertTrue(homework.canSeeSubmissions())
+                }
+            }
+
+            describe("making submissions not public") {
+                beforeEach {
+                    homework.publicSubmissions = false
+                }
+                it("user should not be able to see submissions") {
+                    assertFalse(homework.canSeeSubmissions())
+                }
+            }
+        }
     }
 })

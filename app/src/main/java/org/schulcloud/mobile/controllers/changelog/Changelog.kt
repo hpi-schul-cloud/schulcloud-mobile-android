cfd903817cdf1a5cbe2d6a3498ec2ce5d24270e0
Jonas Wanke <contact@jonas-wanke.com>
2019-03-20 15:09:35 +0100

    ui(main): change changelog title

--- app/src/main/java/org/schulcloud/mobile/controllers/changelog/Changelog.kt
+++ app/src/main/java/org/schulcloud/mobile/controllers/changelog/Changelog.kt
@@ -2,6 +2,8 @@ package org.schulcloud.mobile.controllers.changelog
 
 import com.michaelflisar.changelog.ChangelogBuilder
 import com.michaelflisar.changelog.ChangelogSetup
+import org.schulcloud.mobile.BuildConfig
+import org.schulcloud.mobile.R
 import org.schulcloud.mobile.controllers.base.BaseActivity
 import org.schulcloud.mobile.utils.DarkModeUtils
 
@@ -17,6 +19,7 @@ object Changelog {
     fun showDialog(activity: BaseActivity, showOnlyNew: Boolean = true) {
         ChangelogBuilder().apply {
             withRenderer(Renderer())
+            withTitle(activity.getString(R.string.changelog_title, BuildConfig.VERSION_NAME))
             if (showOnlyNew)
                 withManagedShowOnStart(true)
         }.buildAndShowDialog(activity, DarkModeUtils.getInstance(activity).isActive)

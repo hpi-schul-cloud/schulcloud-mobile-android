1067d561c5d29dda57092aaa916b2585bf2770d6
Andrea Nathansen <andrea.nathansen@student.hpi.uni-potsdam.de>
2019-04-01 09:08:54 +0200

    test: add test for MainActivity

---
+++ app/src/androidTest/java/org/schulcloud/mobile/controllers/main/MainActivityTest.kt
@@ -0,0 +1,55 @@
+package org.schulcloud.mobile.controllers.main
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.BundleMatchers.hasEntry
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtras
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.hamcrest.CoreMatchers.*
+import org.junit.Rule
+import org.junit.Test
+import org.schulcloud.mobile.R
+import org.schulcloud.mobile.SchulCloudApp
+import org.schulcloud.mobile.commonTest.MainActivityIntentTestRule
+
+class MainActivityTest {
+    private val appContext = ApplicationProvider.getApplicationContext<SchulCloudApp>()
+
+    @Rule
+    @JvmField
+    val activityRule = MainActivityIntentTestRule()
+
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun shouldShowBottomAppBarOnDefault() {
+        onView(withId(R.id.bottomAppBar)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun shouldShareLinkWhenShareClicked() {
+        val intent = Intent()
+        val result = Instrumentation.ActivityResult(Activity.RESULT_OK, intent)
+        intending(allOf(hasAction(equalTo(Intent.ACTION_CHOOSER)))).respondWith(result)
+
+        onView(withId(R.id.base_action_share)).perform(click())
+
+        intended(allOf(
+                hasExtras(allOf(
+                        hasEntry(equalTo(Intent.EXTRA_INTENT), hasAction(Intent.ACTION_SEND)),
+                        hasEntry(equalTo(Intent.EXTRA_TITLE), containsString(appContext.getString(R.string.share_title))))),
+                hasAction(equalTo(Intent.ACTION_CHOOSER))))
+    }
+}
---
+++ app/src/commonTest/java/org.schulcloud.mobile/commonTest/MainActivityIntentTestRule.kt
@@ -0,0 +1,41 @@
+package org.schulcloud.mobile.commonTest
+
+import org.schulcloud.mobile.controllers.main.MainActivity
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import io.mockk.*
+import org.schulcloud.mobile.models.event.Event
+import org.schulcloud.mobile.models.event.EventRepository
+import org.schulcloud.mobile.models.homework.Homework
+import org.schulcloud.mobile.models.homework.HomeworkRepository
+import org.schulcloud.mobile.models.news.News
+import org.schulcloud.mobile.models.news.NewsRepository
+import org.schulcloud.mobile.models.user.UserRepository
+import org.schulcloud.mobile.storages.Onboarding
+import org.schulcloud.mobile.utils.asLiveData
+
+class MainActivityIntentTestRule : IntentsTestRule<MainActivity>(MainActivity::class.java) {
+    override fun beforeActivityLaunched() {
+        super.beforeActivityLaunched()
+
+        mockkStatic(UserRepository::class)
+        every { UserRepository.isAuthorized } returns true
+        mockkObject(Onboarding)
+        // To avoid the TapTargetView of the NavigationDrawer
+        every { Onboarding.navigation } returns mockk {
+            every { getUpdates() } returns null
+        }
+
+        // mock repositories
+        mockkObject(NewsRepository, EventRepository, HomeworkRepository)
+        every { EventRepository.eventsForToday(any()) } returns emptyList<Event>().asLiveData()
+        every { HomeworkRepository.openHomeworkForNextWeek(any()) } returns emptyList<Homework>().asLiveData()
+        every { NewsRepository.newsList(any()) } returns emptyList<News>().asLiveData()
+    }
+
+    override fun afterActivityFinished() {
+        super.afterActivityFinished()
+
+        clearAllMocks()
+    }
+}
+

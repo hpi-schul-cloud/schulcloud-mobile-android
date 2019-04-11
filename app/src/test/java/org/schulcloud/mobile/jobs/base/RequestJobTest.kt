f3204cf9198cdeff6cbc98c94412988447ced7dd
Andrea Nathansen <andrea.nathansen@student.hpi.uni-potsdam.de>
2019-04-11 17:11:29 +0200

    test: fix RequestJobTest

--- app/src/test/java/org/schulcloud/mobile/jobs/base/RequestJobTest.kt
+++ app/src/test/java/org/schulcloud/mobile/jobs/base/RequestJobTest.kt
@@ -22,17 +22,16 @@ import org.schulcloud.mobile.network.FeathersResponse
 import org.schulcloud.mobile.commonTest.course
 import org.schulcloud.mobile.utils.NetworkUtil
 import retrofit2.Response
+import ru.gildor.coroutines.retrofit.awaitResponse
 
-//@RunWith(RobolectricTestRunner::class)
-//@Config(application = SchulCloudTestApp::class)
 class RequestJobTest {
     val mockApiService = mockk<ApiServiceInterface>()
     val courses = courseList(3)
     val course = course("id")
     val toDelete: (RealmQuery<Course>.() -> RealmQuery<Course>) = { this }
     val callback = spyk<RequestJobCallback>()
-    val mockSyncData = mockk<Sync.Data<Course>>()
-    val call = mockk<(ApiServiceInterface.() -> Call<FeathersResponse<List<Course>>>)>()
+    val mockSyncData = mockk<Sync.Data<Course>>(relaxUnitFun = true)
+    val apiServiceCall: ApiServiceInterface.() -> Call<FeathersResponse<List<Course>>> = { listUserCourses() }
     val feathersResponse = FeathersResponse<List<Course>>().apply {
         data = courses
     }
@@ -43,21 +42,22 @@ class RequestJobTest {
     @Before
     fun setUp() {
         Dispatchers.setMain(mainThreadSurrogate)
+
         mockkObject(ApiService, NetworkUtil, Sync.Data)
-        mockkStatic(UserRepository::class)
-        mockkStatic(android.util.Log::class)
-        every { Log.w(ofType<String>(), ofType<String>()) } returns 0
-        every { Log.w(ofType<String>(), ofType<String>(), any()) } returns 0
-        every { Log.i(ofType<String>(), ofType<String>()) } returns 0
+        mockkStatic(UserRepository::class, android.util.Log::class)
+        mockkStatic("ru.gildor.coroutines.retrofit.CallAwaitKt")
+
+        every { Log.w(ofType(), ofType<String>()) } returns 0
+        every { Log.w(ofType(), ofType(), any()) } returns 0
+        every { Log.i(ofType(), ofType()) } returns 0
 
         every { ApiService.getInstance() } returns mockApiService
         every { NetworkUtil.isOnline() } returns true
         every { UserRepository.isAuthorized } returns true
         every { Sync.Data.with(Course::class.java, courses, toDelete) } returns mockSyncData
-        every { mockSyncData.run() } just runs
-        every { call(mockApiService)} returns mockk{
+        every { mockApiService.apiServiceCall() } returns mockk {
             every { execute() } returns mockResponse
-            //coEvery { awaitResponse() } returns  mockResponse
+            coEvery { awaitResponse() } returns  mockResponse
         }
         every { mockResponse.body() } returns feathersResponse
         every { mockResponse.isSuccessful } returns true
@@ -71,18 +71,18 @@ class RequestJobTest {
     }
 
     @Test
-    fun shouldCallApiServiceWhenOnlineAndNoAuthRequired(){
+    fun shouldCallApiServiceWhenOnlineAndNoAuthRequired() {
         runBlocking {
-            RequestJob.Data.with(call, toDelete, callback).run()
+            RequestJob.Data.with(apiServiceCall, toDelete, callback).run()
         }
-        verify { mockApiService.call() }
+        verify { mockApiService.apiServiceCall() }
     }
 
     @Test
     fun shouldCallNoAuthErrorWhenAuthRequiredAndNotAuthorized() {
         every { UserRepository.isAuthorized } returns false
         runBlocking {
-            RequestJob.Data.with(call, toDelete, callback, RequestJob.Precondition.AUTH).run()
+            RequestJob.Data.with(apiServiceCall, toDelete, callback, RequestJob.Precondition.AUTH).run()
         }
         verify { callback.error(RequestJobCallback.ErrorCode.NO_AUTH) }
     }
@@ -91,56 +91,54 @@ class RequestJobTest {
     fun shouldCallApiServiceWhenAuthRequiredAndAuthorized() {
         every { UserRepository.isAuthorized } returns true
         runBlocking {
-            RequestJob.Data.with(call, toDelete, callback, RequestJob.Precondition.AUTH).run()
+            RequestJob.Data.with(apiServiceCall, toDelete, callback, RequestJob.Precondition.AUTH).run()
         }
-        verify { mockApiService.call() }
+        verify { mockApiService.apiServiceCall() }
     }
 
     @Test
     fun shouldCallApiServiceWhenAuthNotRequiredAndNotAuthorized() {
         every { UserRepository.isAuthorized } returns false
         runBlocking {
-            RequestJob.Data.with(call, toDelete, callback).run()
+            RequestJob.Data.with(apiServiceCall, toDelete, callback).run()
         }
-        verify { mockApiService.call() }
+        verify { mockApiService.apiServiceCall() }
     }
 
     @Test
-    fun shouldCallNoNetworkErrorWhenNotOnline(){
+    fun shouldCallNoNetworkErrorWhenNotOnline() {
         every { NetworkUtil.isOnline() } returns false
         runBlocking {
-            RequestJob.Data.with(call, toDelete, callback).run()
+            RequestJob.Data.with(apiServiceCall, toDelete, callback).run()
         }
         verify { callback.error(RequestJobCallback.ErrorCode.NO_NETWORK) }
     }
 
-    //TODO: fix those tests
     @Test
-    fun shouldCallSyncWhenApiServiceCallSuccessfulAndDataNotNull(){
+    fun shouldCallSyncWhenApiServiceCallSuccessfulAndDataNotNull() {
         every { mockResponse.body() } returns feathersResponse
         every { mockResponse.isSuccessful } returns true
         runBlocking {
-            RequestJob.Data.with(call, toDelete, callback).run()
+            RequestJob.Data.with(apiServiceCall, toDelete, callback).run()
         }
-        //verify { callback.error(RequestJobCallback.ErrorCode.ERROR) }
-       verify { Sync.Data.with(Course::class.java, courses, toDelete) }
+        verify { Sync.Data.with(Course::class.java, courses, toDelete) }
     }
 
     @Test
-    fun shouldCallErrorAndNotCallSyncWhenApiServiceCallNotSuccessful(){
+    fun shouldCallErrorAndNotCallSyncWhenApiServiceCallNotSuccessful() {
         every { mockResponse.isSuccessful } returns false
         runBlocking {
-            RequestJob.Data.with(call, toDelete, callback).run()
+            RequestJob.Data.with(apiServiceCall, toDelete, callback).run()
         }
         verify { callback.error(RequestJobCallback.ErrorCode.ERROR) }
         verify { mockSyncData wasNot Called }
     }
 
     @Test
-    fun shouldCallErrorAndNotCallSyncWhenDataNull(){
+    fun shouldCallErrorAndNotCallSyncWhenDataNull() {
         every { mockResponse.body() } returns feathersResponseWithoutData
         runBlocking {
-            RequestJob.Data.with(call, toDelete, callback).run()
+            RequestJob.Data.with(apiServiceCall, toDelete, callback).run()
         }
         verify { callback.error(RequestJobCallback.ErrorCode.ERROR) }
         verify { mockSyncData wasNot Called }

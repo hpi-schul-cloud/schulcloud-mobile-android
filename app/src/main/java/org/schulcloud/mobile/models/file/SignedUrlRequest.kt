7ee5485373fe76efa2740634a89bb0b102cecb93
Andrea Nathansen <andrea.nathansen@student.hpi.uni-potsdam.de>
2019-06-11 20:09:44 +0200

    refactor(SignedUrlRequest): remove unused companion object

--- app/src/main/java/org/schulcloud/mobile/models/file/SignedUrlRequest.kt
+++ app/src/main/java/org/schulcloud/mobile/models/file/SignedUrlRequest.kt
@@ -5,11 +5,6 @@ package org.schulcloud.mobile.models.file
  * Date: 7/5/2018
  */
 class SignedUrlRequest {
-    companion object {
-        val ACTION_GET = "getObject"
-        val ACTION_PUT = "putObject"
-    }
-
     var parent: String? = null
     var filename: String? = null
     var fileType: String? = null
